9c7eae639d886224048302abcfecc5ab5f9f38db
Tamara Cook <10754072+tamaracha@users.noreply.github.com>
2019-12-11 17:46:13 +0100

    Travis: Adapt credential variables

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -3,10 +3,10 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew -x test build
+  ./gradlew build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew -x test -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PsonatypeUsername="${sonatypeUsername}" -PsonatypePassword="${sonatypePassword}" build bintrayUpload -PreleaseMode=full --stacktrace --info
+  ./gradlew build && ./gradlew -Pbintray_user="${bintray_user}" -Pbintray_key="${bintray_key}" bintrayUpload
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
   ./gradlew build
